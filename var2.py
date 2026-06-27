27bebc9f1015ceab738fa771d6b3a4ea427e05f0
CH.Gamana <gamana2905@gmail.com>
2026-06-27 17:23:08 +0530

    files

---
+++ var2.py
@@ -0,0 +1,14 @@
+age = 23
+old = False
+a = None
+
+print(type(age)) #int
+print(type(old)) #bool
+print(type(a)) #NoneType
+
+b = 23.5
+
+print(type(b)) #float
+
+#Keywords -- reserved words in python
+#python keywords are case sensitive
--- variables.py
+++ variables.py
@@ -1,9 +1,34 @@
 name = "Gamana" #string 
-age = "21"
+age = 21
 print("My name is", name, "and my age is", age)
 print(name,age)
 print(name)
 print(age)
 
 # each variable is having some memory
-print(id(name))
+print(id(name)) #memory location of variable name
+
+age2 = age
+print(id(age2)) #memory location of variable age2
+print(id(age)) #memory location of variable age
+print("Memory location of age and age2 are same", id(age2) == id(age)) #True
+print("Memory location of name and age are same", id(name) == id(age)) #False
+print(age)
+print(age2)
+print("Memory location of name and age2 are same", id(name) == id(age2)) #False
+
+print(type(name)) #string
+print(type(age)) #string
+print(type(age2)) #string
+
+price = 100.50
+print(type(price)) #float
+a = 'gamana'
+b = "gamanaaa"
+c = '''gamanaaaa'''
+print(type(a)) #string  
+print(type(b)) #string
+print(type(c)) #string
+print(a)
+print(b)
+print(c)
