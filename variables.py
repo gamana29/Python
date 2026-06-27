3f03723e14e26dc22796a9a86350fa8281f09c5d
CH.Gamana <gamana2905@gmail.com>
2026-06-27 17:08:40 +0530

    files

--- variables.py
+++ variables.py
@@ -1,6 +1,9 @@
-name = "Gamana"
+name = "Gamana" #string 
 age = "21"
 print("My name is", name, "and my age is", age)
 print(name,age)
 print(name)
 print(age)
+
+# each variable is having some memory
+print(id(name))
