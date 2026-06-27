c742f6710860a31c552ecd30a524b659e8cc9ddc
CH.Gamana <gamana2905@gmail.com>
2026-06-27 18:14:07 +0530

    files

---
+++ practice.py
@@ -0,0 +1,16 @@
+a = int(input("enter number1: "))
+b = int(input("enter num2: "))
+sum = a+b
+print(sum)
+
+
+side = float(input("enter side: "))
+print("Area of square is", side*side)
+
+c = float(input("enter n1:"))
+d = float(input("enter n2:"))
+print((c+d)/2)
+
+e = int(input("enter n1:"))
+f = int(input("enter n2:"))
+print(e>=f)
