e69784239f60382066e207d497fede5c2c5e77bb
CH.Gamana <gamana2905@gmail.com>
2026-06-27 18:05:07 +0530

    files

---
+++ input.py
@@ -0,0 +1,24 @@
+name = input("enter your name:")
+print("Welcome", name)
+
+#input() -- string
+print(type(name)) #string
+
+print("Enter your age:")
+age = input() #string
+print("Your age is", age)
+age = int(input()) #int
+print("Your age is", age)
+age = float(input()) #float
+print("Your age is", age)
+
+
+name = input("enter name: ")
+age = input("enter age: ")
+marks = input("enter marks:")
+print("Welcome")
+print(name)
+print(age)
+print(marks)
+
+
---
+++ typeconversion.py
@@ -0,0 +1,21 @@
+#automatic type convertion in python
+
+#type conversion
+
+a = 2
+b = 4.25
+sum = a+b
+print(sum)
+
+#type conversion - automatic type conversion
+#float is higher than int, so int is converted to float and then addition is performed
+
+c = "2"
+d = 3.2
+e = int(c)
+sum2 = e+d
+print(sum2)
+
+f = int("2")
+g = float("3.2")
+print(f+g)
