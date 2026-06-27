b86266e1ee7b44e526029b20661dfb20a3d449b8
CH.Gamana <gamana2905@gmail.com>
2026-06-27 17:40:52 +0530

    files

---
+++ operators.py
@@ -0,0 +1,53 @@
+#Arithmetic Operators
+
+a = 5
+b = 2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b) #floor division
+print(a%b) #modulus
+print(a**b) #exponential a^b
+
+#Realtional Operators
+a = 5
+b = 2
+print(a>b) #True
+print(a<b) #False
+print(a>=b) #True
+print(a<=b) #False
+print(a==b) #False
+print(a!=b) #True
+print(a is b) #False
+print(a is not b) #True
+print(a is a) #True
+print(a is not a) #False
+print(a is 5) #True
+print(a is not 5) #False
+print(a is 2) #False
+print(a is not 2) #True
+
+#Assignment Operators
+a = 5
+b = 2
+a += b # a = a + b
+num = 10
+num = num+10
+print(num)
+num-=5
+print(num)
+
+#Logical Operators
+a = True
+b = False
+print(a and b) #False
+print(a or b) #True
+print(not a) #False
+c = 50
+b = 30
+print(c>40)
+print(b<30)
+print(c>40 and b<30) #False
+print(c>40 or b<30) #True
+print(not(c>40 and b<30)) #True
---
+++ printsum.py
@@ -0,0 +1,18 @@
+a = 2
+b = 5
+print(a+b)
+print("Sum of a and b is", a+b)
+sum = a+b
+print("Sum of a and b is", sum)
+print(sum)
+diff  = a-b
+print("Difference of a and b is", diff)
+print(diff)
+
+
+"""
+Multi line comment
+
+"""
+
+##Single line comment
