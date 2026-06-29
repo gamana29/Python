08708eb1d5de6e1938777a126c6181ee6ae6b463
CH.Gamana <gamana2905@gmail.com>
2026-06-29 10:37:10 +0530

    files

---
+++ loops.py
@@ -0,0 +1,23 @@
+#Loops 
+#Loops are used to iterate over a sequence of items. There are two types of loops in Python: for loops and while loops.
+#Loops are used to execute a block of code repeatedly until a certain condition is met.
+
+
+#while loop
+c = 1
+while c<=5:
+    print("Hello")
+    c+=1
+print(c)
+
+
+i = 0
+while i<=5:
+    print("pathu",i)
+    i+=1
+print(i)
+
+i = 5
+while i>=1:
+    print(i)
+    i-=1
