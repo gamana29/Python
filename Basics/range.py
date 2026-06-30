eda00bddd6afb8553e0fea7568008daa9e56c20e
CH.Gamana <gamana2905@gmail.com>
2026-06-30 22:20:29 +0530

    updated

---
+++ Basics/range.py
@@ -0,0 +1,26 @@
+#range - returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and stops before a specified number.
+#Syntax: range(start, stop, step)
+
+# range(10) #0 to 9
+
+# list = [1,2,3,4,5,6,7,8,9]
+# for i in range(4):
+#     print(i) 
+
+# seq = range(29)
+# for i in seq:
+#     print(i)
+
+# #range(start, stop, step)
+# for i in range(1, 10, 2):
+#     print(i)
+
+
+for i in range(10):
+    print(i)
+
+for i in range(1,5):
+    print(i)
+
+for i in range(1,10,3):
+    print(i)
