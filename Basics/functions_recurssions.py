74e06c54ba1910c99922aa28ae3b08d3f239d5f8
CH.Gamana <gamana2905@gmail.com>
2026-06-30 21:21:25 +0530

    functions and recurssions

---
+++ Basics/functions_recurssions.py
@@ -0,0 +1,16 @@
+#functions 
+
+def add(a,b):
+    return a+b  
+print(add(2,3))
+
+
+def sn(a,b):
+    sum = a+b
+    return sum
+print(sn(5,23))
+print(sn(2,2))
+print(sn(5,23))
+print(sn(2,2))
+print(sn(5,23))
+print(sn(2,2))
---
+++ Basics/practice5.py
@@ -0,0 +1,45 @@
+for i in range(101):
+    print(i)
+
+for i in range(100,0,-1):
+    print(i)
+
+for i in range(1,11):
+    print(3*i)
+
+
+#pass statement
+
+for i in range(5):
+    pass #pass statement is used as a placeholder for future code, when the code is not yet implemented, it does nothing and can be used to avoid errors.
+print("Hello")
+
+
+n = 5
+s = 0
+for i in range(n):
+    s+=i
+print(s)
+
+n = 7
+s=0
+i=1
+while i<=n:
+    s+=i
+    i+=1
+print(s)
+
+#factorial
+fact = 1
+n = 5
+for i in range(1,n+1):
+    fact*=i
+print(fact)
+
+fact = 1
+n = 6
+i=1
+while i<=n:
+    fact*=i
+    i+=1
+print(fact)
