63c80999ff2db1880a7d60123fb96608d7fd0bc6
CH.Gamana <gamana2905@gmail.com>
2026-06-30 20:25:44 +0530

    files

--- practice3.py
+++ practice3.py
@@ -25,8 +25,17 @@
 # while nums:
 #     print(nums.pop())
 
-nums = [1,4,9,16,25,36,49,64,81,100]
-idx = 0
-while idx < len(nums):
-    print(nums[idx])
-    idx+=1
+# nums = [1,4,9,16,25,36,49,64,81,100]
+# idx = 0
+# while idx < len(nums):
+#     print(nums[idx])
+#     idx+=1
+
+
+nums = (1,4,9,16,25,36,49,64,81,100)
+x = int(input("enter number:"))
+i = 0
+while i< len(nums):
+    if (nums[i] == x):
+        print("found")
+    i+=1
