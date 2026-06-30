5062a9407b4013b4c5e54eb47c9979d44bc64876
CH.Gamana <gamana2905@gmail.com>
2026-06-30 20:41:33 +0530

    loops

---
+++ for_loop.py
@@ -0,0 +1,24 @@
+#for loop
+
+list1 = [1,2,3,4,5]
+for i in list1:
+    print(i)
+
+
+veg = ["tamatoo","potato","onion"]
+for n in veg:
+    print(n)
+
+tuple = (1,2,3,4)
+for r in tuple:
+    print(r)
+
+
+s1 = "gamana"
+for c in s1:
+    if (c =="a"):
+        print("found")
+        break
+    print(c) 
+
+    print("loop is completed")
---
+++ practice4.py
@@ -0,0 +1,12 @@
+nums = [1,4,9,16,25,36,49,64,81,100]
+for i in nums:
+    print(i)
+
+
+nums =(1,4,9,16,25,36,49,64,81,100)
+x = 49
+i = 0
+for el in nums:
+    if(el ==x):
+        print("found",i)
+    i+=1
