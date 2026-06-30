4c850a20653e361c8d18d2fc6e275e3461fcf8e3
CH.Gamana <gamana2905@gmail.com>
2026-06-30 20:13:27 +0530

    files

---
+++ practice3.py
@@ -0,0 +1,32 @@
+
+# 1 to 100
+# i = 1
+# while i<=100:
+#     print(i)
+#     i+=1
+
+
+# 100 to 1
+# i = 100
+# while i>=1:
+#     print(i)
+#     i-=1
+
+#Multiplication table of a number
+# n = int(input("enter number:"))
+# i = 1
+# while i<=10:
+#     print(n*i)
+#     i+=1
+
+#q4
+
+# nums = [1,4,9,16,25,36,49,64,81,100]
+# while nums:
+#     print(nums.pop())
+
+nums = [1,4,9,16,25,36,49,64,81,100]
+idx = 0
+while idx < len(nums):
+    print(nums[idx])
+    idx+=1
